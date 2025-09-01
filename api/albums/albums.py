8219743b5a0b5db3d765f5990c87d194ec236e51
anirudhanishal <129587573+anirudhanishal@users.noreply.github.com>
2025-09-01 17:42:54 +0530

    Update albums.py

--- api/albums/albums.py
+++ api/albums/albums.py
@@ -1,43 +1,49 @@
 import asyncio
 
 class Albums:
-    async def search_albums(self, search_query: str, limit: int) -> list:
+    async def search_albums(self, search_query: str, limit: int) -> dict:
         aiohttp = self.aiohttp
         endpoints = self.api_endpoints
         errors = self.errors
         response = await aiohttp.post(endpoints.search_albums_url + search_query)
         result = await response.json()
         album_ids = []
-        for i in range(0,int(limit)):
+        for i in range(0, int(limit)):
             try:
                 album_ids.append(result['gr'][0]['gd'][int(i)]['seo'])
             except (IndexError, TypeError, KeyError):
                 pass
-        if len(album_ids) == 0:
-          return await errors.no_results()
-        album_info = await self.get_album_info(album_ids, False)
-        return album_info
 
-    async def get_album_info(self, album_id: list, info: bool) -> list:
+        if len(album_ids) == 0:
+            return await errors.no_results()
+
+        return await self.get_album_info(album_ids, False)
+
+    async def get_album_info(self, album_id: list, info: bool) -> dict:
         aiohttp = self.aiohttp
         endpoints = self.api_endpoints
         album_info = []
-        if info == True:
+        if info is True:
             self.info = True
-        for i in album_id:
-          response = await aiohttp.post(endpoints.album_details_url + i)
-          result = await response.json()
-          album_info.extend(await asyncio.gather(*[self.format_json_albums(result) for i in range(0,1)]))
-        return album_info
 
-    async def get_album_tracks(self, album_id: list) -> list:
+        for i in album_id:
+            response = await aiohttp.post(endpoints.album_details_url + i)
+            result = await response.json()
+            album_info.extend(await asyncio.gather(
+                *[self.format_json_albums(result) for _ in range(0, 1)]
+            ))
+
+        return {
+            "success": True,
+            "data": album_info
+        }
+
+    async def get_album_tracks(self, album_id: str) -> list:
         aiohttp = self.aiohttp
         endpoints = self.api_endpoints
         response = await aiohttp.post(endpoints.album_details_url + album_id)
         result = await response.json()
-        track_seokeys = []
-        for i in result['tracks']:
-            track_seokeys.append(i['seokey'])
+        track_seokeys = [i['seokey'] for i in result['tracks']]
         result = await self.get_track_info(track_seokeys)
         return result
 
@@ -49,6 +55,7 @@ class Albums:
             data['seokey'] = results['album']['seokey']
         except (IndexError, TypeError, KeyError):
             return await errors.no_results()
+
         data['album_id'] = results['album']['album_id']
         data['title'] = results['album']['title']
         try:
@@ -59,23 +66,31 @@ class Albums:
             data['artists'] = ""
             data['artist_seokeys'] = ""
             data['artist_ids'] = ""
+
         data['duration'] = results['album']['duration']
         data['is_explicit'] = await functions.isExplicit(results['album']['parental_warning'])
         data['language'] = results['album']['language']
         data['label'] = results['album']['recordlevel']
         data['track_count'] = results['album']['trackcount']
+
         try:
             data['release_date'] = results['album']['release_date']
-        except:
+        except KeyError:
             data['release_date'] = ""
+
         data['play_count'] = results['album']['al_play_ct']
         data['favorite_count'] = results['album']['favorite_count']
         data['album_url'] = f"https://gaana.com/album/{results['album']['seokey']}"
-        data['images'] = {'urls': {}}
-        data['images']['urls']['large_artwork'] = (results['album']['artwork']).replace("size_s.jpg", "size_l.jpg")
-        data['images']['urls']['medium_artwork'] = (results['album']['artwork']).replace("size_s.jpg", "size_m.jpg")
-        data['images']['urls']['small_artwork'] = (results['album']['artwork'])
-        if self.info == True:
-            data['tracks'] =  await self.get_album_tracks(data['seokey'])
+        data['images'] = {
+            'urls': {
+                'large_artwork': results['album']['artwork'].replace("size_s.jpg", "size_l.jpg"),
+                'medium_artwork': results['album']['artwork'].replace("size_s.jpg", "size_m.jpg"),
+                'small_artwork': results['album']['artwork']
+            }
+        }
+
+        if getattr(self, "info", False) is True:
+            data['tracks'] = await self.get_album_tracks(data['seokey'])
+
         self.info = False
         return data
